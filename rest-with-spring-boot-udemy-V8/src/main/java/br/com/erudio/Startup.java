5a5bd76b803863d0c621f1004c437583f9b2ab7e
DevIngrid <juliedoisj@gmail.com>
2021-07-27 03:58:17 -0300

    Verbo Patch

---
+++ rest-with-spring-boot-udemy-V8/src/main/java/br/com/erudio/Startup.java
@@ -0,0 +1,20 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+//@EnableAutoConfiguration
+//@ComponentScan
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+		
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
+		String result = bCryptPasswordEncoder.encode("7771777");
+		System.out.println("Minha senha " + result);
+	}
+
+}
---
+++ rest-with-spring-boot-udemy-V8/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (2,'Uberlandia - Minas Gerais - Brasil','Gabriela','Female','Costa');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (3,'Uberlandia - Minas Gerais - Brasil','Rafael','Male','Silva');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (5,'Uberlandia - Minas Gerais - Brasil','Flavio','Male','Costa');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (7,'Uberlandia - Minas Gerais - Brasil','Fernanda','Female','Cardoso');
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES (8,'Uberlandia - Minas Gerais - Brasil','Leandro','Male','Costa');
