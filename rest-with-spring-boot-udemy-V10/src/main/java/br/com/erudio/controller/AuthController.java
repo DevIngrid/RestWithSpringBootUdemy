06fdf20e41bbc26c0fe994c246574008d3baed41
DevIngrid <juliedoisj@gmail.com>
2021-08-06 02:52:38 -0300

    Upload files

---
+++ rest-with-spring-boot-udemy-V10/src/main/java/br/com/erudio/controller/AuthController.java
@@ -0,0 +1,68 @@
+package br.com.erudio.controller;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.com.erudio.data.model.User;
+import br.com.erudio.security.AccountCredentialsVO;
+import br.com.erudio.security.jwt.JwtTokenProvider;
+import br.com.erudio.services.UserServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "AuthenticationEndPoint")
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+	
+	@Autowired
+	JwtTokenProvider tokenProvider;
+	
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	UserServices services;
+	
+	
+	@SuppressWarnings("rawtypes")
+	@Operation(summary = "Authenticates a user and returns a token")
+	@PostMapping(value="/signin", produces = {"application/json", "application/xml", "application/x-yaml"},
+				consumes = {"application/json", "application/xml", "application/x-yaml"})
+	public ResponseEntity signin(@RequestBody AccountCredentialsVO data) {
+		try {
+			
+			String username = data.getUsername();
+			
+			String token = "";
+			
+			User usuarioAutenticado = (User) services.autenticar(data);
+			token = tokenProvider.createToken(username, usuarioAutenticado.getRoles());
+			
+			
+			Map<Object, Object> model = new HashMap<>();
+			model.put("username", username);
+			model.put("token", token);
+			
+			return ok(model);
+			
+					
+		} catch (UsernameNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+			}
+	}
+
+}
